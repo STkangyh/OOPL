c308ef803f8cbbacd73f2ccec6f19ed471819c3f
YH <yhkang2003@gmail.com>
2023-05-04 17:10:40 +0900

    9th Class #2

--- OOPL/src/practice2.java
+++ OOPL/src/practice2.java
@@ -1,42 +1,57 @@
+import java.io.*;
 
 public class practice2 {
-    public static void main(String[] args) {
-        Queue queue = new Queue();
-        for (int i = 1; i <=20; i++) {
-            queue.enqueue(i);
-        }
-        while (!queue.empty()) {
-            System.out.print(queue.dequeue() + " ");
+    public static void main(String[] args) throws IOException {
+        practice1.GeometricObjectComparable[] objects = {
+                new Square(2), new practice1.Circle(5), new Square(5), new practice1.Rectangle(3, 4), new Square(4.5)
+        };
+        for (int i=0; i<5; i++){
+            System.out.println("Area is "+objects[i].getArea());
+            if (!objects[i].isFilled())
+                objects[i].howToColor();
         }
     }
-    public static class Queue{
-        private int[] element;
-        private int size=0;
-        public Queue(){
-            element = new int[8];
+
+    public static interface Colorable {
+        public abstract void howToColor();
+    }
+
+    public static class Square extends practice1.GeometricObjectComparable implements Colorable {
+        protected double a;
+        public Square(double a){
+            this.a=a;
+            setFilled(false);
         }
-        public void enqueue(int v){
-            if (size>=getSize()){
-                int[] temp = new int[element.length*2];
-                System.arraycopy(element,0,temp,0,element.length);
-                element=temp;
-            }
-            element[size++]=v;
+        public double getA() {
+            return a;
         }
-        public int dequeue(){
-            int temp;
-            for(int i=0;i<size;i++){
-                temp=element[i];
-                element[i]=element[i+1];
-                element[i+1]=temp;
-            }
-            return element[size-1];
+
+        @Override
+        public double getArea() {
+            return a*a;
         }
-        public boolean empty(){
-            return element[0]==element[element.length-1];
+
+        public void setA(double a) {
+            this.a = a;
         }
-        public int getSize(){
-            return element.length;
+
+        @Override
+        public double getPerimeter() {
+            return 4*a;
+        }
+
+        public void howToColor() {
+            System.out.println("Color all four sides");
+        }
+
+        @Override
+        public void setColor(String color) {
+            super.setColor(color);
+        }
+
+        @Override
+        public String getColor() {
+            return super.getColor();
         }
     }
 }
